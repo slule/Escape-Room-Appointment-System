77db740a5f4b9b42531b393034e5b2f03ae32144
shalo <shalo@schmoozerpc.mshome.net>
2024-04-09 20:43:15 -0500

    Initial

---
+++ EscapeRoomAppointmentSystem/src/main/java/dmacc/edu/EscapeRoomAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package dmacc.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeRoomAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapeRoomAppointmentSystemApplication.class, args);
+	}
+
+}
---
+++ EscapeRoomAppointmentSystem/src/test/java/dmacc/edu/EscapeRoomAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dmacc.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeRoomAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
